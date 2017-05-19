bcc45599ea3e756dd5ebcbe51857d72aab512ffc
Chip Huyen <huyenn@stanford.edu>
2017-05-19 10:28:04 -0700

    code for 2nd class

---
+++ 02_queue_example.py
@@ -0,0 +1,45 @@
+""" Example to demonstrate how to use queues
+Author: Chip Huyen
+Prepared for O'Reilly "A short course in TensorFlow"
+"""
+from __future__ import print_function
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+import numpy as np
+import tensorflow as tf
+
+N_SAMPLES = 1000
+NUM_THREADS = 4
+# Generating some simple data
+# create 1000 random samples, each is a 1D array from the normal distribution (10, 1)
+data = 10 * np.random.randn(N_SAMPLES, 4) + 1 
+# create 1000 random labels of 0 and 1
+target = np.random.randint(0, 2, size=N_SAMPLES) 
+
+queue = tf.FIFOQueue(capacity=50, dtypes=[tf.float32, tf.int32], shapes=[[4], []])
+
+enqueue_op = queue.enqueue_many([data, target])
+data_sample, label_sample = queue.dequeue()
+
+# create ops that do something with data_sample and label_sample
+
+# create NUM_THREADS to do enqueue
+qr = tf.train.QueueRunner(queue, [enqueue_op] * NUM_THREADS)
+with tf.Session() as sess:
+	# create a coordinator, launch the queue runner threads.
+	coord = tf.train.Coordinator()
+	enqueue_threads = qr.create_threads(sess, coord=coord, start=True)
+	try:
+		for step in range(100): # do to 100 iterations
+			if coord.should_stop():
+				break
+			data_batch, label_batch = sess.run([data_sample, label_sample])
+			print(data_batch)
+			print(label_batch)
+	except Exception as e:
+		coord.request_stop(e)
+	finally:
+		coord.request_stop()
+		coord.join(enqueue_threads)
---
+++ 02_word2vec_starter.py
@@ -0,0 +1,104 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.contrib.tensorboard.plugins import projector
+
+from process_data import process_data
+
+VOCAB_SIZE = 50000
+BATCH_SIZE = 128
+EMBED_SIZE = 128 # dimension of the word embedding vectors
+SKIP_WINDOW = 1 # the context window
+NUM_SAMPLED = 64    # Number of negative examples to sample.
+LEARNING_RATE = 1.0
+NUM_TRAIN_STEPS = 20000
+SKIP_STEP = 2000 # how many steps to skip before reporting the loss
+
+def word2vec(batch_gen):
+    """ Build the graph for word2vec model and train it """
+    # Step 1: define the placeholders for input and output
+    # center_words have to be int to work on embedding lookup
+
+    # TO DO
+    with tf.name_scope('data'):
+        center_words = tf.placeholder(tf.int32, [BATCH_SIZE], name='center_words')
+        target_words = tf.placeholder(tf.int32, [BATCH_SIZE, 1], name='target_words')
+
+    # Step 2: define weights. In word2vec, it's actually the weights that we care about
+    # vocab size x embed size
+    # initialized to random uniform -1 to 1
+
+    # TOO DO
+    with tf.name_scope('embedding_matrix'):
+        embed_matrix = tf.Variable(tf.random_uniform([VOCAB_SIZE, EMBED_SIZE], -1.0, 1.0, name='embed_matrix'))
+
+    # Step 3: define the inference
+    # get the embed of input words using tf.nn.embedding_lookup
+    # embed = tf.nn.embedding_lookup(embed_matrix, center_words, name='embed')
+
+    # TO DO
+    # with tf.name_scope('embedding_lookup'):
+    #     embed = tf.nn.embedding_lookup(embed_matrix, center_words, name='embed')
+
+    # Step 4: construct variables for NCE loss
+    # tf.nn.nce_loss(weights, biases, labels, inputs, num_sampled, num_classes, ...)
+    # nce_weight (vocab size x embed size), intialized to truncated_normal stddev=1.0 / (EMBED_SIZE ** 0.5)
+    # bias: vocab size, initialized to 0
+
+    # TO DO
+    with tf.name_scope('nce_loss'):
+        embed = tf.nn.embedding_lookup(embed_matrix, center_words, name='embed')
+        nce_weights = tf.Variable(tf.truncated_normal([VOCAB_SIZE, EMBED_SIZE], 
+                                                      stddev=1.0 / (EMBED_SIZE ** 0.5),
+                                                      name='nce_weights'))
+        nce_biases = tf.Variable(tf.zeros([VOCAB_SIZE]), name='nce_biases')
+
+        # define loss function to be NCE loss function
+        # tf.nn.nce_loss(weights, biases, labels, inputs, num_sampled, num_classes, ...)
+        # need to get the mean accross the batch
+        # note: you should use embedding of center words for inputs, not center words themselves
+
+        # TO DO
+        loss = tf.reduce_mean(tf.nn.nce_loss(weights=nce_weights, 
+                                             biases=nce_biases, 
+                                             labels=target_words, 
+                                             inputs=embed, 
+                                             num_sampled=NUM_SAMPLED, 
+                                             num_classes=VOCAB_SIZE, 
+                                             name='loss'))
+
+    # Step 5: define optimizer
+    
+    # TO DO
+    with tf.name_scope('optimizer'):
+        optimizer = tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(loss)
+
+    with tf.Session() as sess:
+        # TO DO: initialize variables
+        sess.run(tf.global_variables_initializer())
+
+        total_loss = 0.0 # we use this to calculate the average loss in the last SKIP_STEP steps
+        writer = tf.summary.FileWriter('./graphs/no_frills/', sess.graph)
+        for index in range(NUM_TRAIN_STEPS):
+            centers, targets = next(batch_gen)
+            # TO DO: create feed_dict, run optimizer, fetch loss_batch
+            _, loss_batch = sess.run([optimizer, loss], feed_dict={center_words: centers, target_words: targets})
+
+            total_loss += loss_batch
+            if (index + 1) % SKIP_STEP == 0:
+                print('Average loss at step {}: {:5.1f}'.format(index, total_loss / SKIP_STEP))
+                total_loss = 0.0
+        writer.close()
+
+def main():
+    batch_gen = process_data(VOCAB_SIZE, BATCH_SIZE, SKIP_WINDOW)
+    word2vec(batch_gen)
+
+if __name__ == '__main__':
+    main()
